843e204a1654c30535e62a2790eda9bb5af2a317
pedrohjdev <pedrohjdev@gmail.com>
2024-04-30 08:15:23 -0300

    Add: Configuração da Pasta Destino do FTP

--- index.js
+++ index.js
@@ -12,47 +12,50 @@ const ftpConfig = {
   host: process.env.FTP_HOST,
   user: process.env.FTP_USER,
   password: process.env.FTP_PASSWORD,
+
 };
 
-// Set storage engine for multer
+//-->>Inicializa a configuração do Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/') // Save uploaded files to the 'uploads' folder
+    cb(null, 'uploads/') 
   },
   filename: function (req, file, cb) {
     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
   }
 });
 
-// Initialize multer upload
 const upload = multer({ storage: storage });
 
-// Upload endpoint
 app.post('/upload', upload.single('file'), async (req, res) => {  
   try {
     const client = new ftp.Client();
     await client.access(ftpConfig);
+    
+    //-->> Configura a pasta destino 
+    await client.ensureDir("/Arquivos")
+
+    //-->> Define o local do arquivo de origem e o nome do arquivo originial
     await client.uploadFrom(req.file.path, req.file.originalname);
     await client.close();
-    res.json({ message: 'File uploaded successfully' });
+    res.json({ message: 'Upload realizado com sucesso' });
   } catch (error) {
     console.error('FTP Error:', error);
-    res.status(500).json({ error: 'Failed to upload file to FTP server' });
+    res.status(500).json({ error: 'Falha no Upload do FTP' });
   }
   console.log(req.file, 'req');
 });
 
-// Download endpoint
 app.get('/download/:filename', (req, res) => {
   const fileName = req.params.filename;
   res.download(path.join(__dirname, 'uploads', fileName), (err) => {
     if (err) {
-      res.status(404).json({ error: 'File not found' });
+      res.status(404).json({ error: 'Arquivo não encontrado' });
     }
   });
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
